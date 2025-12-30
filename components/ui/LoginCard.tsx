409016b6a639f0b2df2898f94fb8100b8b9dde83
Balaji Uggina <ugginabalaji2003@gmail.com>
2025-12-30 19:38:42 +0530

    Revert "UnReverted:"Getting my commmited and pushed changes back ""
    This reverts commit 6f4bb3db92b65691e4cb3f5de9416edc31a84fc5.

--- components/ui/LoginCard.tsx
+++ components/ui/LoginCard.tsx
@@ -9,7 +9,7 @@ export default function LoginCard() {
         {/* Logo Section */}
         <div className="flex items-center ">
           <div className=" text-red-600 font-black px-2 py-1 text-4xl">GK</div>
-          <div className="block leading-none">
+          <div className="block leading-tight">
             <p className="font-normal text-gray-800 text-lg">GraceKennedy</p>
             <p className="text-lg text-gray-800 font-normal">
               General Insurance
